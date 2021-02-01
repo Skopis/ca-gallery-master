94009450b46815652ef5341585d191dfaa764e54
Lian Skopis <lian.skopis@gmail.com>
2021-02-01 22:36:29 +0200

    after render and pics change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,68 @@
 console.log('Starting up');
+
+
+
+function initPage() {
+renderPage();
+}
+
+function renderPage() {
+var projs = getProjs();
+var srtHTMLModals = '';
+var srtHTMLPortfolio = '';
+for (var i = 0; i < projs.length; i++) {
+    var currproj=projs[i];
+    srtHTMLModals +=
+    `<div class="portfolio-modal modal fade"
+    id="portfolioModal${i + 1}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <div class="modal-body">
+                            <h2>${currproj.name}</h2>
+                            <p class="item-intro text-muted">${currproj.title}</p>
+                            <img class="img-fluid d-block mx-auto" src="img/portfolio/0${i + 1}-full.jpg" alt="">
+                            <p>${currproj.desc}</p>
+                            <ul class="list-inline">
+                                <li>Date: ${currproj.publishedAt}</li>
+                                <li><a href="${currproj.url}" target="_blank">App Link</a></li>
+                            </ul>
+                            <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                <i class="fa fa-times"></i>
+                                Close Project</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div> `
+
+
+    srtHTMLPortfolio +=
+    `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${i + 1}">
+            <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                    <i class="fa fa-plus fa-3x"></i>
+                </div>
+            </div>
+            <img class="img-fluid" src="img/portfolio/0${i + 1}-thumbnail.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+            <h4>${currproj.name}</h4>
+            <p class="text-muted">???</p>
+        </div>
+    </div>`
+    };
+
+    document.querySelector('.modals-container').innerHTML = srtHTMLModals;
+    document.querySelector('#portfolio-container').innerHTML = srtHTMLPortfolio;
+    }
---
+++ js/proj-service.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var gProjs = [
+    {
+        id: "minesweeper",
+        name: "Minesweeper",
+        title: "Better not explode Motherf*&%$",
+        desc: "A soothing zen game for the whole family, count how many times each of you dies.",
+        url: "https://skopis.github.io/Minesweeper/",
+        publishedAt: "January 21, 2021",
+        labels: ["matrixes", "death"]
+    },
+    {
+        id: "bookshop",
+        name: "Book Shop",
+        title: "Manage the book shop of your dreams",
+        desc: "Own a library? have a very boring life? This app was made for you, loser.",
+        url: "https://skopis.github.io/book-shop/",
+        publishedAt: "January 29, 2021",
+        labels: ["disappointment", "mvc"]
+    },
+    {
+        id: "todos",
+        name: "Todos",
+        title: "Why don't you do something?",
+        desc: "Are you a procrastinator? Well, that means you won't get far in life. This app sure won't change you, nothing will; but it'll make you feel a bit less worthless",
+        url: "https://skopis.github.io/todos-list/",
+        publishedAt: "January 27, 2021",
+        labels: ["Procrastination", "keyboard events"]
+    },
+    {
+        id: "chess",
+        name: "Chess",
+        title: "A Chess game that even you can win at",
+        desc: "Not smart anough to learn how to play chess? This dumbed-down game is just for you! Feel, for the first time in your life, how it feels like not to lose.",
+        url: "https://skopis.github.io/chess/",
+        publishedAt: "January 21, 2021",
+        labels: ["table render", "weakness of the brain"]
+    },
+    {
+        id: "touch-nums",
+        name: "Touch Nums",
+        title: "Can you even count?",
+        desc: "Easier than wearing socks. If you fail this, ask a 2-year old to help you out.",
+        url: "https://skopis.github.io/touch-nums/",
+        publishedAt: "January 14, 2021",
+        labels: ["multi-level support", "easy AF"]
+    },
+    {
+        id: "pacman",
+        name: "Pacman",
+        title: "Here, you take it",
+        desc: "It's full of bugs so you never actually know if you won, lost, or the game broke. enjoy.",
+        url: "https://skopis.github.io/pacman/",
+        publishedAt: "January 21, 2021",
+        labels: ["Matrixes", "keyboard events"]
+    },
+]
+
+function getProjs() {
+    return gProjs;
+}
